4886f61f6006145b14055fdfa9a4b7894de06777
MustafaElAntrawy <mustafaelantrawy@gmail.com>
2021-10-22 17:04:46 +0200

    Solving Homework2 Ex5_Check Whether a Character is an Alphabet or not_

---
+++ Unit_2_C_Programming/Lesson_3/Assignment 2/Check Whether a Character is an Alphabet or not/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	char input;
+	printf("Enter a character: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%c", &input);
+
+	if ((input >= 'a' && input <= 'z') ||(input >= 'A' && input <= 'Z')){
+		printf ("%c is an alphabet", input);
+	}
+	else printf ("%c is not an alphabet", input);
+
+	return 0;
+}
