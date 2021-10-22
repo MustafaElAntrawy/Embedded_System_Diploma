7bdb3e0a3797566de16b396ec42f30f7a42f9e99
MustafaElAntrawy <mustafaelantrawy@gmail.com>
2021-10-22 15:21:06 +0200

    Solving Homework1 Ex3_Add Two Integers_

---
+++ Unit_2_C_Programming/Lesson_3/Assignment_1/Add Two Integers/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+	int x1, x2;
+	printf("Enter two integers: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d %d", &x1, &x2);
+	printf("sum: %d", x1 + x2);
+	return 0;
+}
+
+
