3a9dc375d2058004638372b852b092712b447b61
MustafaElAntrawy <mustafaelantrawy@gmail.com>
2021-10-22 16:38:06 +0200

    Solving Homework2 Ex4Check Whether a Number is Positive or Negative__

---
+++ Unit_2_C_Programming/Lesson_3/Assignment 2/Check Whether a Number is Positive or Negitive/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+	float a;
+	printf("Enter a numbers: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%f", &a);
+
+	if(a == 0) printf ("You entered zero.");
+	else if (a > 0) printf("%.6g is positive", a);
+	else printf("%.6g is negative", a);
+
+	return 0;
+}
