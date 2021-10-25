d198a18fab0cb319c6f8a7c0b847020a7309f8df
MustafaElAntrawy <mustafaelantrawy@gmail.com>
2021-10-25 12:49:41 +0200

    Solving Unit2 homework3_Strings Ex2_Find the Length of a String_

---
+++ Unit_2_C_Programming/Lesson_4/Assignment 3_Strings/Find the Length of a String/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+	char text[100];
+	printf("Enter a String: ");
+	fflush(stdin); fflush(stdout);
+	gets(text);
+	int i;
+	for (i = 0; text[i] != 0; ++i);
+	printf("Length of string: %d", i);
+
+
+	return 0;
+}
