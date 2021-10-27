5f7b0fb36e552f01acdb912f5dabd2d967b379af
MustafaElAntrawy <mustafaelantrawy@gmail.com>
2021-10-27 15:07:52 +0200

    Solving Unit2 homework4_functions Ex2_Calculate Factorial of a Number_

---
+++ Unit_2_C_Programming/Lesson 5/Assignment 4/Calculate Factorial of a Number/main.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+long calculate_factorial(int n);
+
+int main(){
+	int n;
+	printf("Enter a positive integer: ");
+	fflush(stdin); fflush(stdout);
+	scanf("%d", &n);
+	printf("Factorial of %d = %lu", n, calculate_factorial(n));
+	return 0;
+}
+
+long calculate_factorial(int n){
+	if (n <= 1){
+		return 1;
+	}
+	return n * calculate_factorial(n - 1);
+}
